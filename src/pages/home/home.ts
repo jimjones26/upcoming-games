36fa13ea85ff486efe7c444a13e5996614064b78
Jimmy Jones <jimjones26@gmail.com>
2017-12-04 13:59:04 -0500

    get games json pulled into home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 import { DataProvider } from '../../providers/data/data';
 
@@ -18,10 +18,47 @@ import { DataProvider } from '../../providers/data/data';
 export class HomePage {
 
   games: any = [];
+  genre: '5';
+  genreName: string = 'Upcoming Games';
+  favorites = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private _data: DataProvider, private storage: Storage) {
-    this._data.getGames('5', 0)
-      .subscribe(res => this.games = res);
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    private _data: DataProvider,
+    private storage: Storage,
+    public loadingCtrl: LoadingController) {
+
+    let loading = this.loadingCtrl.create({
+      content: 'Getting games...'
+    });
+
+    loading.present().then(() => {
+      this.storage.get('genre')
+        .then((val) => {
+          if (val) {
+            this.genre = val.id;
+            this.genreName = val.name;
+          } else {
+            this.genreName = 'Shooter';
+            this.storage.set('genre', this.genre);
+          }
+
+          this._data.getGames('5', 0)
+            .subscribe(res => {
+              this.games = res;
+              loading.dismiss();
+            });
+        });
+
+      this.storage.get('favorites').then((val) => {
+        if (!val)
+          this.storage.set('favorites', this.favorites);
+        else
+          this.favorites = val;
+      });
+
+    });
+
   }
 
   ionViewDidLoad() {
